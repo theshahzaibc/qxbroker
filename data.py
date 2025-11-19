30c4db7f24a638fe7fe561d9ee649491d8ed0e7d
Shahzaib <shahzaibsaifullah@gmail.com>
2025-11-19 23:12:49 +0500

    ARTICLE 5

--- data.py
+++ data.py
@@ -2,7 +2,8 @@ articles = [
     {"url": "how-to-make-quotex-account-in-pakistan", "page": "ar1.html"},
     {"url": "how-to-verify-quotex-account-in-pakistan", "page": "ar2.html"},
     {"url": "quotex-minimum-deposit-in-pakistan-2025", "page": "ar3.html"},
-    {"url": "quotex-otc-1-minute-sureshot-strategy-2026", "page": "ar4.html"}
+    {"url": "quotex-otc-1-minute-sureshot-strategy-2026", "page": "ar4.html"},
+    {"url": "quotex-top-6-1-minute-otc-binary-trading-strategies", "page": "ar5.html"}
 ]
 
 sitemap = {
