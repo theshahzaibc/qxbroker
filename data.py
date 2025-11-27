2149efb1194138da2609ebc555960a2758d40983
Shahzaib <shahzaibsaifullah@gmail.com>
2025-11-27 15:44:10 +0500

    ARTICLE 7

--- data.py
+++ data.py
@@ -4,7 +4,9 @@ articles = [
     {"url": "quotex-minimum-deposit-in-pakistan-2025", "page": "ar3.html"},
     {"url": "quotex-otc-1-minute-sureshot-strategy-2026", "page": "ar4.html"},
     {"url": "quotex-top-6-1-minute-otc-binary-trading-strategies", "page": "ar5.html"},
-    {"url": "quotex-live-real-market-open-time-pakistan", "page": "ar6.html"}]
+    {"url": "quotex-live-real-market-open-time-pakistan", "page": "ar6.html"},
+    {"url": "how-to-trade-on-order-black-with-100-accuracy-strategy", "page": "ar7.html"}
+]
 
 sitemap = {
     "sitemap.xml": "sitemap.xml",
