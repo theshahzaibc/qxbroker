8568349d0f520cf0aef157a68fc3b316690f24bd
Shahzaib <shahzaibsaifullah@gmail.com>
2025-12-02 01:31:37 +0500

    ARTICLE 10

--- data.py
+++ data.py
@@ -7,7 +7,8 @@ articles = [
     {"url": "quotex-live-real-market-open-time-pakistan", "page": "ar6.html"},
     {"url": "how-to-trade-on-order-black-with-100-accuracy-strategy", "page": "ar7.html"},
     {"url": "how-to-withdraw-from-quotex-in-pakistan", "page": "ar8.html"},
-    {"url": "quotex-withdrawal-problems-in-pakistan", "page": "ar9.html"}
+    {"url": "quotex-withdrawal-problems-in-pakistan", "page": "ar9.html"},
+    {"url": "quotex-pakistan-time-zone", "page": "ar10.html"}
 ]
 
 sitemap = {
